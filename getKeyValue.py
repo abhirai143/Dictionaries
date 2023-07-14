8ea0986199abca036719ebfe629bba10506eb9bd
Abhishek Raibagi <91134432+abhirai143@users.noreply.github.com>
2023-07-14 23:32:34 +0530

    Create getKeyValue.py

---
+++ getKeyValue.py
@@ -0,0 +1,23 @@
+-----------------access KEYS---------------------------
+
+statesAndCapitals = {
+    'Maharashtra': 'Mumbai',
+    'Rajasthan': 'Jaipur',
+}
+ 
+print('List Of given states:\n')
+for state in statesAndCapitals:
+    print(state)
+
+----------------Get Values---------------
+
+statesAndCapitals = {
+    'Gujarat': 'Gandhinagar',
+    'Maharashtra': 'Mumbai',
+    'Rajasthan': 'Jaipur',
+    'Bihar': 'Patna'
+}
+ 
+print('List Of given capitals:\n')
+for capital in statesAndCapitals.values():
+    print(capital)
